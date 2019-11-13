20ee8462315388fb5773b9f70d5db2212608eb86
wangkuntian <773491807@qq.com>
2019-11-13 15:47:38 +0800

    Two Sums

---
+++ leetcode/lessons/easy/001_two_sum/__init__.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+__project__ =  'leetcode'
+__file__    =  '__init__.py.py'
+__author__  =  'king'
+__time__    =  '2019/11/13 15:20'
+
+
+                              _ooOoo_
+                             o8888888o
+                             88" . "88
+                             (| -_- |)
+                             O\  =  /O
+                          ____/`---'\____
+                        .'  \\|     |//  `.
+                       /  \\|||  :  |||//  \
+                      /  _||||| -:- |||||-  \
+                      |   | \\\  -  /// |   |
+                      | \_|  ''\---/''  |   |
+                      \  .-\__  `-`  ___/-. /
+                    ___`. .'  /--.--\  `. . __
+                 ."" '<  `.___\_<|>_/___.'  >'"".
+                | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+                \  \ `-.   \_ __\ /__ _/   .-` /  /
+           ======`-.____`-.___\_____/___.-`____.-'======
+                              `=---='
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                       佛祖保佑        永无BUG
+"""
+
+from leetcode.utils.timeutils import time_interval
+
+'''
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+示例:
+    给定 nums = [2, 7, 11, 15], target = 9
+    因为 nums[0] + nums[1] = 2 + 7 = 9
+    所以返回 [0, 1] 
+'''
+
+
+class Solution(object):
+
+    @time_interval
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash_map = {}
+        for index, num in enumerate(nums):
+            if target - num in hash_map:
+                return [hash_map[target - num], index, ]
+            hash_map[num] = index
+
+
+nums = [2, 7, 11, 15]
+target = 9
+print(Solution().twoSum(nums, target))
