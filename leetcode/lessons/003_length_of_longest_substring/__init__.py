8bfea9ca2ab2e55c3aa2fa7aa4d90ba5077a6c3f
wangkuntian <773491807@qq.com>
2019-11-14 17:49:15 +0800

    Length Of Longest Substring

---
+++ leetcode/lessons/003_length_of_longest_substring/__init__.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+__project__ =  'leetcode'
+__file__    =  '__init__.py.py'
+__author__  =  'king'
+__time__    =  '2019/11/14 15:10'
+
+
+                              _ooOoo_
+                             o8888888o
+                             88" . "88
+                             (| -_- |)
+                             O\  =  /O
+                          ____/`---'\____
+                        .'  \\|     |//  `.
+                       /  \\|||  :  |||//  \
+                      /  _||||| -:- |||||-  \
+                      |   | \\\  -  /// |   |
+                      | \_|  ''\---/''  |   |
+                      \  .-\__  `-`  ___/-. /
+                    ___`. .'  /--.--\  `. . __
+                 ."" '<  `.___\_<|>_/___.'  >'"".
+                | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+                \  \ `-.   \_ __\ /__ _/   .-` /  /
+           ======`-.____`-.___\_____/___.-`____.-'======
+                              `=---='
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                       佛祖保佑        永无BUG
+"""
+from leetcode.utils.timeutils import time_interval
+
+'''
+难度：中等
+
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+    输入: "abcabcbb"
+    输出: 3 
+    解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+    
+示例 2:
+    输入: "bbbbb"
+    输出: 1
+    解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+    
+示例 3:
+    输入: "pwwkew"
+    输出: 3
+    解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+         请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+'''
+
+
+class Solution(object):
+    @time_interval
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        last_seen = {}
+        start = 0
+        longest = 0
+
+        for index, c in enumerate(s):
+            if c in last_seen and last_seen[c] >= start:
+                start = last_seen[c] + 1
+            else:
+                longest = max(longest, index - start + 1)
+            last_seen[c] = index
+        return longest
+
+
+print(Solution().lengthOfLongestSubstring('aa'))
