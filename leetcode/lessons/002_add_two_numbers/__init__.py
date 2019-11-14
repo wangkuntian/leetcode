e17dd45da8ebf59129decdc86d5ca323c07ac6d1
wangkuntian <773491807@qq.com>
2019-11-14 15:07:48 +0800

    add two numbers

---
+++ leetcode/lessons/002_add_two_numbers/__init__.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+__project__ =  'leetcode'
+__file__    =  '__init__.py.py'
+__author__  =  'king'
+__time__    =  '2019/11/13 15:50'
+
+
+                              _ooOoo_
+                             o8888888o
+                             88" . "88
+                             (| -_- |)
+                             O\  =  /O
+                          ____/`---'\____
+                        .'  \\|     |//  `.
+                       /  \\|||  :  |||//  \
+                      /  _||||| -:- |||||-  \
+                      |   | \\\  -  /// |   |
+                      | \_|  ''\---/''  |   |
+                      \  .-\__  `-`  ___/-. /
+                    ___`. .'  /--.--\  `. . __
+                 ."" '<  `.___\_<|>_/___.'  >'"".
+                | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+                \  \ `-.   \_ __\ /__ _/   .-` /  /
+           ======`-.____`-.___\_____/___.-`____.-'======
+                              `=---='
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                       佛祖保佑        永无BUG
+"""
+from leetcode.utils.timeutils import time_interval
+
+'''
+难度：中等
+
+给出两个 非空 的链表用来表示两个非负的整数。
+其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+示例：
+    输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+    输出：7 -> 0 -> 8
+    原因：342 + 465 = 807
+
+'''
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        s = str(self.val) + ' --> '
+        node = self.next
+        while node.next:
+            s += str(node.val) + ' --> '
+            node = node.next
+        s += str(node.val)
+        return s
+
+
+class Solution(object):
+    @time_interval
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        pre = result = ListNode(None)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            pre.next = ListNode(carry % 10)
+            pre = pre.next
+            carry //= 10
+        return result.next
+
+
+l1 = ListNode(2)
+l2 = ListNode(4)
+l3 = ListNode(3)
+l1.next = l2
+l2.next = l3
+
+l4 = ListNode(5)
+l5 = ListNode(6)
+l6 = ListNode(4)
+l4.next = l5
+l5.next = l6
+
+print(Solution().addTwoNumbers(l1, l4))
