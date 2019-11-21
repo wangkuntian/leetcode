5f524e7ed4ce95acbd858220f45739046e3e409d
wangkuntian <773491807@qq.com>
2019-11-21 18:08:39 +0800

    Linked List

--- leetcode/lessons/linked_list/092_reverse_between/__init__.py
+++ leetcode/lessons/linked_list/092_reverse_between/__init__.py
@@ -30,6 +30,7 @@ __time__    =  '2019/11/18 16:54'
 """
 from leetcode.lessons.linked_list import ListNode
 from leetcode.utils.timeutils import time_interval
+
 '''
 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
 
@@ -72,6 +73,43 @@ class Solution(object):
 
         return result.next
 
+    @time_interval
+    def reverseBetween2(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+
+        cur, pre = head, None
+
+        while m > 1:
+            pre = cur
+            cur = cur.next
+            m -= 1
+            n -= 1
+
+        tail, con = cur, pre
+        while n:
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+            n -= 1
+        if con:
+            con.next = pre
+        else:
+            head = pre
+
+        tail.next = cur
+        return head
+
 
 l1 = ListNode.generate([1, 2, 3, 4, 5])
 print(Solution().reverseBetween(l1, 2, 4))
+
+l2 = ListNode.generate([1, 2, 3, 4, 5])
+print(Solution().reverseBetween2(l2, 2, 4))
