3f8facb2891af02a7457efb73a237e896301b15c
wangkuntian <773491807@qq.com>
2020-01-07 11:59:07 +0800

    Array

---
+++ leetcode/lessons/array/561_array_partition_i/__init__.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+__project__ =  'leetcode'
+__file__    =  '__init__.py.py'
+__author__  =  'king'
+__time__    =  '2020/1/2 17:05'
+
+
+                              _ooOoo_
+                             o8888888o
+                             88" . "88
+                             (| -_- |)
+                             O\  =  /O
+                          ____/`---'\____
+                        .'  \\|     |//  `.
+                       /  \\|||  :  |||//  \
+                      /  _||||| -:- |||||-  \
+                      |   | \\\  -  /// |   |
+                      | \_|  ''\---/''  |   |
+                      \  .-\__  `-`  ___/-. /
+                    ___`. .'  /--.--\  `. . __
+                 ."" '<  `.___\_<|>_/___.'  >'"".
+                | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+                \  \ `-.   \_ __\ /__ _/   .-` /  /
+           ======`-.____`-.___\_____/___.-`____.-'======
+                              `=---='
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                       佛祖保佑        永无BUG
+"""
+
+"""
+难度：简单
+
+给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
+
+示例 1:
+输入: [1,4,3,2]
+输出: 4
+解释: n 等于 2, 最大总和为 4 = min(1, 2) + min(3, 4).
+
+提示:
+n 是正整数,范围在 [1, 10000].
+数组中的元素范围在 [-10000, 10000].
+"""
+
+
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        i = 0
+        result = 0
+        while True:
+            result += min(nums[i:i + 2])
+            i += 2
+            if i > len(nums) - 1:
+                break
+        return result
+
+    def arrayPairSum_2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        sort_nums = sorted(nums)
+        return sum(sort_nums[::2])
+
+
+print(Solution().arrayPairSum([7, 3, 1, 0, 0, 6]))
+print(Solution().arrayPairSum_2([7, 3, 1, 0, 0, 6]))
+
+enumerate
