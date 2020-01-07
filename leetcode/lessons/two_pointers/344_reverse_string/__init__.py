3e95ebcd4a15e71c299b3df25209285dfb0f2de2
wangkuntian <773491807@qq.com>
2020-01-07 11:57:33 +0800

    Two Pointers

---
+++ leetcode/lessons/two_pointers/344_reverse_string/__init__.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+__project__ =  'leetcode'
+__file__    =  '__init__.py.py'
+__author__  =  'king'
+__time__    =  '2020/1/2 16:44'
+
+
+                              _ooOoo_
+                             o8888888o
+                             88" . "88
+                             (| -_- |)
+                             O\  =  /O
+                          ____/`---'\____
+                        .'  \\|     |//  `.
+                       /  \\|||  :  |||//  \
+                      /  _||||| -:- |||||-  \
+                      |   | \\\  -  /// |   |
+                      | \_|  ''\---/''  |   |
+                      \  .-\__  `-`  ___/-. /
+                    ___`. .'  /--.--\  `. . __
+                 ."" '<  `.___\_<|>_/___.'  >'"".
+                | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+                \  \ `-.   \_ __\ /__ _/   .-` /  /
+           ======`-.____`-.___\_____/___.-`____.-'======
+                              `=---='
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                       佛祖保佑        永无BUG
+"""
+
+"""
+难度：简单
+
+编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+
+不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+
+你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+
+示例 1：
+输入：["h","e","l","l","o"]
+输出：["o","l","l","e","h"]
+
+示例 2：
+输入：["H","a","n","n","a","h"]
+输出：["h","a","n","n","a","H"]
+"""
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        l = len(s)
+        for i in range(0, l // 2):
+            temp = s[i]
+            s[i] = s[l - 1 - i]
+            s[l - 1 - i] = temp
+        print(s)
+
+    def reverseString_2(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+
+        while left <= right:
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left += 1
+            right -= 1
+
+        print(s)
+
+
+Solution().reverseString(["h", "e", "l", "l", "o"])
+Solution().reverseString_2(["h", "e", "l", "l", "o"])
---
+++ leetcode/lessons/two_pointers/485_max_consecutive_ones/__init__.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+__project__ =  'leetcode'
+__file__    =  '__init__.py.py'
+__author__  =  'king'
+__time__    =  '2020/1/7 11:21'
+
+
+                              _ooOoo_
+                             o8888888o
+                             88" . "88
+                             (| -_- |)
+                             O\  =  /O
+                          ____/`---'\____
+                        .'  \\|     |//  `.
+                       /  \\|||  :  |||//  \
+                      /  _||||| -:- |||||-  \
+                      |   | \\\  -  /// |   |
+                      | \_|  ''\---/''  |   |
+                      \  .-\__  `-`  ___/-. /
+                    ___`. .'  /--.--\  `. . __
+                 ."" '<  `.___\_<|>_/___.'  >'"".
+                | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+                \  \ `-.   \_ __\ /__ _/   .-` /  /
+           ======`-.____`-.___\_____/___.-`____.-'======
+                              `=---='
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                       佛祖保佑        永无BUG
+"""
+
+"""
+难度：简单
+
+给定一个二进制数组， 计算其中最大连续1的个数。
+
+示例 1:
+输入: [1,1,0,1,1,1]
+输出: 3
+解释: 开头的两位和最后的三位都是连续1，所以最大连续1的个数是 3.
+
+注意：
+输入的数组只包含 0 和1。
+输入数组的长度是正整数，且不超过 10,000。
+"""
+
+
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        max_result = 0
+        for index, num in enumerate(nums):
+            if num == 1:
+                result += 1
+            else:
+                max_result = max(result, max_result)
+                result = 0
+
+        return max(result, max_result)
+
+
+print(Solution().findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]))
