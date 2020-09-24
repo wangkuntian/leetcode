ad266911f9708425801ebacb8edbf4b3c5569085
wangkuntian <773491807@qq.com>
2020-09-24 10:44:30 +0800

    Binary Search Tree

---
+++ leetcode/lessons/binary_search_tree/538_convert_bst_to_greater_tree/__init__.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+__project__ =  'leetcode'
+__file__    =  '__init__.py.py'
+__author__  =  'king'
+__time__    =  '2020/9/21 11:26'
+
+
+                              _ooOoo_
+                             o8888888o
+                             88" . "88
+                             (| -_- |)
+                             O\  =  /O
+                          ____/`---'\____
+                        .'  \\|     |//  `.
+                       /  \\|||  :  |||//  \
+                      /  _||||| -:- |||||-  \
+                      |   | \\\  -  /// |   |
+                      | \_|  ''\---/''  |   |
+                      \  .-\__  `-`  ___/-. /
+                    ___`. .'  /--.--\  `. . __
+                 ."" '<  `.___\_<|>_/___.'  >'"".
+                | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+                \  \ `-.   \_ __\ /__ _/   .-` /  /
+           ======`-.____`-.___\_____/___.-`____.-'======
+                              `=---='
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                       佛祖保佑        永无BUG
+"""
+from leetcode.lessons.binary_search_tree import TreeNode
+
+"""
+难度：简单
+
+给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+
+例如：
+
+输入: 原始二叉搜索树:
+              5
+            /   \
+           2     13
+
+输出: 转换为累加树:
+             18
+            /   \
+          20     13
+
+"""
+
+
+class Solution(object):
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        self.val = 0
+        self.dfs(root)
+        return root
+
+    def dfs(self, node):
+        if not node:
+            return
+        self.dfs(node.right)
+        node.val += self.val
+        self.val = node.val
+        self.dfs(node.left)
+
+
+t = TreeNode(5)
+t1 = TreeNode(2)
+t2 = TreeNode(13)
+t.left = t1
+t.right = t2
+
+print(Solution().convertBST(t))
+
+
+
